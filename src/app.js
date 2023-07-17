8fd5589b483c03d22f4546326841a02066778ef2
wilypradana <pradanawily28@gmail.com>
2023-07-17 22:10:45 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function dropdown() {
+  document.querySelector("#submenu").classList.toggle("hidden");
+  document.querySelector("#arrow").classList.toggle("rotate-0");
+}
+dropdown();
+
+function openSidebar() {
+  document.querySelector(".sidebar").classList.toggle("hidden");
+}
